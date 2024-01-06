c019a6a43fff432337b6250c85721cbedac8fc0a
Jeanine <jeanine.a.santiago@gmail.com>
2024-01-06 18:06:50 -0300

    Inciando meus primeiros projetos com C#

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Projetosavandes.Models;
+
+Pessoa pessoa1 = new Pessoa();
+pessoa1.Nome = "Joaquim";
+pessoa1.Idade = 10;
+pessoa1.Apresentar();
+
