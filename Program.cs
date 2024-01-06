8a06cf7ba4f9ff9ae67b91c389679a2533fd540d
Jeanine <jeanine.a.santiago@gmail.com>
2024-01-06 20:11:27 -0300

    Menu interativo

--- Program.cs
+++ Program.cs
@@ -1,13 +1,53 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using Projetosavandes.Models;
 
-Calculadora calc = new Calculadora();
 
-calc.Somar(10, 30);
-calc.Subtrair(10, 50);
-calc.Multiplicar(15, 45);
-calc.Dividir(2, 2);
-calc.Potencia(3, 3);
+string opcao;
+
+while(true)
+{
+    Console.Clear();
+    Console.WriteLine("Digite a sua opção:");
+    Console.WriteLine("1- Cadastrar Cliente");
+    Console.WriteLine("2- Buscar cliente");
+    Console.WriteLine("3- Apagar cliente");
+    Console.WriteLine("4- Encerrar");
+
+    opcao = Console.ReadLine();
+
+    switch (opcao)
+    {
+        case "1":
+            Console.WriteLine("Cadastro de cliente");
+            break;
+
+         case "2":
+            Console.WriteLine("Busca de cliente");
+            break;
+
+         case "3":
+            Console.WriteLine("Apagar cliente");
+            break;
+
+         case "4":
+            Console.WriteLine("Encerrar");
+            Environment.Exit(0);
+            break;  
+
+            default:
+            Console.WriteLine("Opção inválida");
+            break;  
+    }
+}
+
+
+// Calculadora calc = new Calculadora();
+
+// calc.Somar(10, 30);
+// calc.Subtrair(10, 50);
+// calc.Multiplicar(15, 45);
+// calc.Dividir(2, 2);
+// calc.Potencia(3, 3);
 
 
 
