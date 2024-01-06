17a975c30dc824dd400a97841e1ef4cb8e7a9f10
Jeanine <jeanine.a.santiago@gmail.com>
2024-01-06 19:23:44 -0300

    Calculadora

--- Program.cs
+++ Program.cs
@@ -1,8 +1,25 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using Projetosavandes.Models;
 
-Pessoa pessoa1 = new Pessoa();
-pessoa1.Nome = "Joaquim";
-pessoa1.Idade = 10;
-pessoa1.Apresentar();
+Calculadora calc = new Calculadora();
+
+calc.Somar(10, 30);
+calc.Subtrair(10, 50);
+calc.Multiplicar(15, 45);
+calc.Dividir(2, 2);
+calc.Potencia(3, 3);
+
+
+
+
+
+
+
+
+
+
+//Pessoa pessoa1 = new Pessoa();
+//pessoa1.Nome = "Joaquim";
+//pessoa1.Idade = 10;
+//pessoa1.Apresentar();
 
